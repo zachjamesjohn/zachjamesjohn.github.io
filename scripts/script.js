34cfcfbcbb94b50bfa15a070578fa43e6a3c2d05
zachjamesjohn <67977743+zachjamesjohn@users.noreply.github.com>
2021-02-11 22:02:41 -0500

    Fixes made after html validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+//script.js
+//COIS 3420 Web Application Development
+//Assignment #1 - Front End Design
+//Author: Zachary John - zjohn
+//Description: The only thing this script does is open the hamburger menu by changing the active class
+
 const toggleButton = document.getElementsByClassName('toggle_button')[0]
 const navbarLinks = document.getElementsByTagName('nav')[0]
 
